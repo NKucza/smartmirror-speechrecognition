c46ce9845e4bb628ffe9ce750094d67a7455a991
Nils Kucza <nkucza@cit-ec.uni-bielefeld.de>
2018-10-11 13:12:59 +0200

    code clean

--- smartmirror-speechrecognition.js
+++ smartmirror-speechrecognition.js
@@ -1,60 +1,13 @@
-/**
- * @file MMM-voice.js
- *
- * @author fewieden
- * @license MIT
- *
- * @see  https://github.com/fewieden/MMM-voice
- */
-
-/* global Module Log MM */
-
-/**
- * @external Module
- * @see https://github.com/MichMich/MagicMirror/blob/master/js/module.js
- */
-
-/**
- * @external Log
- * @see https://github.com/MichMich/MagicMirror/blob/master/js/logger.js
- */
-
-/**
- * @external MM
- * @see https://github.com/MichMich/MagicMirror/blob/master/js/main.js
- */
-
-/**
- * @module MMM-voice
- * @description Frontend for the module to display data.
- *
- * @requires external:Module
- * @requires external:Log
- * @requires external:MM
- */
 Module.register('smartmirror-speechrecognition', {
 
     /** @member {string} icon - Microphone icon. */
     icon: 'fa-microphone-slash',
     /** @member {boolean} pulsing - Flag to indicate listening state. */
     pulsing: true,
-    /** @member {boolean} help - Flag to switch between render help or not. */
-    help: false,
     /**
-     * @member {Object} defaults - Defines the default config values.
-     * @property {int} timeout - Seconds to active listen for commands.
-     * @property {string} keyword - Keyword to activate active listening.
-     * @property {boolean} debug - Flag to enable debug information.
      */
     defaults: {
-        timeout: 15,
-        keyword: 'MAGIC MIRROR',
-        debug: false,
-		show_command_list: ["show", "display", "appear"],
-		hide_command_list: ["remove", "hide"],
-		module_list: [
-			{name : "clock", words : ["clock","time"]}
-		]
+        
     },
 
     /**
@@ -80,21 +33,6 @@ Module.register('smartmirror-speechrecognition', {
      */
     getStyles() {
         return ['font-awesome.css', 'smartmirror-speechrecognition.css'];
-    },
-
-    /**
-     * @function getTranslations
-     * @description Translations for this module.
-     * @override
-     *
-     * @returns {Object.<string, string>} Available translations for this module (key: language code, value: filepath).
-     */
-    getTranslations() {
-        return {
-            en: 'translations/en.json',
-            de: 'translations/de.json',
-            id: 'translations/id.json'
-        };
     },
 
     /**
@@ -128,41 +66,12 @@ Module.register('smartmirror-speechrecognition', {
             voice.appendChild(debug_ger);
         }
 
-        const modules = document.querySelectorAll('.module');
-        for (let i = 0; i < modules.length; i += 1) {
-            if (!modules[i].classList.contains(this.name)) {
-                if (this.help) {
-                    modules[i].classList.add(`${this.name}-blur`);
-                } else {
-                    modules[i].classList.remove(`${this.name}-blur`);
-                }
-            }
-        }
-
        wrapper.appendChild(voice);
 
         return wrapper;
     },
 
-    /**
-     * @function notificationReceived
-     * @description Handles incoming broadcasts from other modules or the MagicMirror core.
-     * @override
-     *
-     * @param {string} notification - Notification name
-     * @param {*} payload - Detailed payload of the notification.
-     */
- /*   notificationReceived(notification, payload) {
-        if (notification === 'DOM_OBJECTS_CREATED') {
-            this.sendSocketNotification('START', { config: this.config, modules: this.modules });
-        } else if (notification === 'REGISTER_VOICE_MODULE') {
-            if (Object.prototype.hasOwnProperty.call(payload, 'mode') && Object.prototype.hasOwnProperty.call(payload, 'sentences')) {
-                this.modules.push(payload);
-            }
-        }
-    }, */
-
-    /**
+     /**
      * @function socketNotificationReceived
      * @description Handles incoming messages from node_helper.
      * @override
